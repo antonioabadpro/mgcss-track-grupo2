b669d077126784f526365d18cf34be62b8f02c17
Antonio Abad <antonioabadpro@gmail.com>
2026-03-13 12:26:42 +0100

    Creación del Proyecto

---
+++ src/main/java/com/mgcss/MgcssTrackGrupo2Application.java
@@ -0,0 +1,13 @@
+package com.mgcss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgcssTrackGrupo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgcssTrackGrupo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgcss/MgcssTrackGrupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgcss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgcssTrackGrupo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
