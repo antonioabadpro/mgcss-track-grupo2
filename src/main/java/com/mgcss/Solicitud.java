69f636d657a75760549829a41d29d66a2818f6b1
Iveree <javitamalc@gmail.com>
2026-03-18 22:33:46 +0100

    fix: Correccion en clase Solicitud

--- src/main/java/com/mgcss/Solicitud.java
+++ src/main/java/com/mgcss/Solicitud.java
@@ -1,17 +1,7 @@
 package com.mgcss;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "solicitud")
 public class Solicitud {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private int estado;
