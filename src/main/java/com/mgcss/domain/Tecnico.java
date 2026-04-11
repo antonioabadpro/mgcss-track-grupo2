3fcb2a68c4b9855ea9a5cd96ea172ce567aed291
Daniel <danielmarchenajimenez2002@gmail.com>
2026-04-11 14:01:58 +0200

    merge: fix/#26-Arreglar-Logica-de-Negocio-y-TESTS
    commit 3037624d14f09b5bb754490024e7889a30744db0
    Author: Antonio Abad <antonioabadpro@gmail.com>
    Date:   Fri Apr 10 20:29:52 2026 +0200
    
        fix:Arreglo la Logica de Negocio e implemento nuevos TESTS
    
        Intento aumentar el coverage de SonarQube Cloud
    
    commit 0a7a6632dbbbf21f3e35615097f929684c80ef61
    Author: Antonio Abad <antonioabadpro@gmail.com>
    Date:   Fri Apr 10 12:49:34 2026 +0200
    
        fix: Arreglo el pom.xml y el Test de la Clase 'TecnicoTest'
    
    commit bb0cc5900494d8aea00ae8e0e75d60df4a333af3
    Author: Antonio Abad <antonioabadpro@gmail.com>
    Date:   Fri Apr 10 12:27:23 2026 +0200
    
        fix: Arreglo de la Clase Tecnico
    
        Modifico los Test de la Clase 'TecnicoTest'

--- src/main/java/com/mgcss/domain/Tecnico.java
+++ src/main/java/com/mgcss/domain/Tecnico.java
@@ -1,10 +1,32 @@
 package com.mgcss.domain;
 
 public class Tecnico {
+    private Long id;
+    
+    String nombre;
+    int edad;
     boolean tecnicoActivo;
+
+    public Tecnico() {
+        this.tecnicoActivo = false;
+    }
+
+    public Tecnico(String nombre, int edad) {
+        this.tecnicoActivo = false;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
 
     public Tecnico(boolean estado) {
         this.tecnicoActivo = estado;
+    }
+
+    public boolean isTecnicoActivo(){
+        return this.tecnicoActivo;
     }
 
     public void setTecnicoActivo(){
@@ -15,8 +37,23 @@ public class Tecnico {
         this.tecnicoActivo = false;
     }
 
-    public boolean isTecnicoActivo(){
-        return this.tecnicoActivo;
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return this.edad;
     }
     
+    public void setEdad(int edad) {
+        if(edad < 18){
+            throw new IllegalArgumentException("El tecnico NO puede ser menor de edad.");
+        }
+        
+        this.edad = edad;
+    }
 }
--- src/test/java/com/mgcss/domain/TecnicoTests.java
+++ src/test/java/com/mgcss/domain/TecnicoTests.java
@@ -1,13 +1,18 @@
 package com.mgcss.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class TecnicoTests {
+    /**
+     * Este test verifica que un técnico activo puede estar asignado a una solicitud.
+     */
     @Test
     void testSoloTecnicoActivoASolicitud() {
         Tecnico tecnico = new Tecnico(true);
@@ -16,6 +21,10 @@ public class TecnicoTests {
 
         assertTrue(isSetted, "El tecnico es activo");
     }
+    
+    /**
+     * Este test verifica que un técnico inactivo NO pueda estar asignado a una solicitud.
+     */
     @Test
     void testSoloTecnicoInactivoASolicitud() {
         Tecnico tecnico = new Tecnico(false);
@@ -23,6 +32,54 @@ public class TecnicoTests {
         boolean isSetted = solicitud.setTecnico(tecnico);
 
         assertFalse(isSetted, "El tecnico es inactivo");
+    }
+
+    /**
+     * Este test verifica que el estado de un técnico pueda cambiarse correctamente entre activo e inactivo.
+     */
+    @Test
+    void testCambiarEstadoTecnico() {
+        Tecnico tecnico = new Tecnico();
+        assertFalse(tecnico.isTecnicoActivo(), "El tecnico debería ser inactivo por defecto");
+
+        tecnico.setTecnicoActivo();
+        assertTrue(tecnico.isTecnicoActivo(), "El tecnico debería ser activo después de llamar a setTecnicoActivo()");
+
+        tecnico.setTecnicoInactivo();
+        assertFalse(tecnico.isTecnicoActivo(), "El tecnico debería ser inactivo después de llamar a setTecnicoInactivo()");
+    }
+
+    /**
+     * Este test verifica que se puedan establecer correctamente el nombre y la edad de un técnico.
+     */
+    @Test
+    void testSetNombreYEdadTecnico() {
+        Tecnico tecnico = new Tecnico();
+        String nombre = "Juan Perez";
+        int edad = 30;
+
+        tecnico.setNombre(nombre);
+        tecnico.setEdad(edad);
+
+        assertEquals(nombre, tecnico.getNombre(), "El nombre del técnico debería ser 'Juan Perez'");
+        assertEquals(edad, tecnico.getEdad(), "La edad del técnico debería ser 30");
+    }
+
+    /**
+     * Este test verifica que NO el Tecnico NO puede ser menor de edad.
+     * Si el Tecnico es menor de edad, se lanza una excepción.
+     */
+    @Test
+    void verificarMayoriaEdadTecnico() {
+        Tecnico tecnico = new Tecnico();
+        int edad = 16;
+
+        try {
+            tecnico.setEdad(edad);
+            fail("Debe ejecutarse la excepción 'IllegalArgumentException', ya que el Tecnico es menor de edad.");
+        } catch (IllegalArgumentException e) {
+            assertEquals("El tecnico NO puede ser menor de edad.", e.getMessage());
+        }
     }
 }
 
