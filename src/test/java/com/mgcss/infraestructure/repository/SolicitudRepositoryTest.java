e147919a5cddecccf0d302441297bab7ddf250a4
Daniel <danielmarchenajimenez2002@gmail.com>
2026-04-24 13:45:15 +0200

    merge: feat/#23-crear-primer-test-de-integracion
    commit 224d338c062c01e71e2244dd7f00c03264fafb4d
    Author: Daniel <danielmarchenajimenez2002@gmail.com>
    Date:   Fri Apr 24 13:44:52 2026 +0200
    
        feat: Test de solicitud creado
    
        El test comprueba guardar y recuperar correctamente una solicitud.

---
+++ src/test/java/com/mgcss/infraestructure/repository/SolicitudRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.mgcss.infraestructure.repository;
+
+import com.mgcss.domain.Solicitud;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("integration")
+@DataJpaTest
+public class SolicitudRepositoryTest {
+
+    @Autowired
+    private SolicitudRepository solicitudRepository;
+
+    /**
+     * Este test verifica que una solicitud pueda ser guardada y recuperada correctamente de la base de datos.
+     */
+    @Test
+    public void testGuardarYRecuperarSolicitud() {
+        // Guardar entidad
+        Solicitud solicitud = new Solicitud();
+        Solicitud solicitudGuardada = solicitudRepository.save(solicitud);
+        
+        // Verificar integridad de que se guardó y tiene ID asignado
+        assertNotNull(solicitudGuardada.getId(), "El ID no debería ser nulo tras guardar");
+
+        // Recuperarla
+        Optional<Solicitud> solicitudRecuperada = solicitudRepository.findById(solicitudGuardada.getId());
+        
+        // Verificar integridad
+        assertTrue(solicitudRecuperada.isPresent(), "La solicitud debería encontrarse en la base de datos");
+        assertEquals(solicitudGuardada.getId(), solicitudRecuperada.get().getId(), "Los IDs deben coincidir");
+        assertEquals(solicitudGuardada.getEstado(), solicitudRecuperada.get().getEstado(), "Los estados deben coincidir");
+        assertEquals(solicitudGuardada.getFechaCreacion(), solicitudRecuperada.get().getFechaCreacion(), "Las fechas deben coincidir");
+    }
+}
