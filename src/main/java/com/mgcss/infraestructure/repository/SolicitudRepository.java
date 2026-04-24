2b157d3c9210805023f80d1ff428d36e8c175194
JavierUHU <163757126+Iveree@users.noreply.github.com>
2026-04-24 12:36:40 +0200

    feat: Implementacion de contrato JPA

---
+++ src/main/java/com/mgcss/infraestructure/repository/SolicitudRepository.java
@@ -0,0 +1,9 @@
+package com.mgcss.infraestructure.repository;
+
+import com.mgcss.domain.Solicitud;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SolicitudRepository extends JpaRepository<Solicitud, Long> {
+}
