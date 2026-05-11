af0f37822e3c78ed0ae036643488655f697af726
Antonio Abad <antonioabadpro@gmail.com>
2026-05-11 13:26:01 +0200

    doc: Documento con Swagger las clases DTO y el SolicitudController
    docs:
    - Documento con Swagger las clases 'SolicitudController', 'EstadoHistoricoDTO', 'SolicitudRequestDTO' y 'SolicitudResponseDTO'
    
    fixes:
    - Corrijo los imports de la Fecha en la clase 'Solicitud'

--- src/main/java/com/mgcss/api/dto/SolicitudRequestDTO.java
+++ src/main/java/com/mgcss/api/dto/SolicitudRequestDTO.java
@@ -1,6 +1,8 @@
 package com.mgcss.api.dto;
 
 import com.mgcss.domain.EstadoSolicitud;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +17,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SolicitudRequestDTO {
+    @Schema(description = "ID del técnico asignado a la solicitud", example = "10")
     private Long tecnicoId;
+    @Schema(description = "Estado de la solicitud que enviamos en la petición", example = "EN_PROCESO")
     private EstadoSolicitud estado;
 }
--- src/main/java/com/mgcss/api/dto/SolicitudResponseDTO.java
+++ src/main/java/com/mgcss/api/dto/SolicitudResponseDTO.java
@@ -1,6 +1,8 @@
 package com.mgcss.api.dto;
 
 import com.mgcss.domain.EstadoSolicitud;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,9 +19,14 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SolicitudResponseDTO {
+    @Schema(description = "ID de la solicitud", example = "1")
     private Long id;
+    @Schema(description = "Estado actual de la solicitud", example = "ABIERTA")
     private EstadoSolicitud estado;
+    @Schema(description = "Fecha de creación de la solicitud", example = "11/05/2026 10:00:00")
     private String fechaCreacion;
+    @Schema(description = "ID del técnico asignado a la solicitud", example = "10")
     private Long tecnicoId;
+    @Schema(description = "Lista que contiene el histórico de estados que ha tenido la solicitud")
     private List<EstadoHistoricoDTO> historicoEstados;
 }
